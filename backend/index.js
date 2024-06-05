cb6642a19cf54988280249f5c6bf2e3d3c8a5385
palcherin <palcherin17@gmail.com>
2024-06-05 21:14:37 +0300

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,38 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
-dotenv.config();
 const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const fs = require("fs");
+
+dotenv.config();
+
 const databaseSeeder = require("./databaseSeeder");
 const userRoute = require("./routes/Users");
 const productRoute = require("./routes/Products");
 const orderRoute = require("./routes/Orders");
 
 // Log the types of the imported routes
-console.log('Type of databaseSeeder:', typeof databaseSeeder); 
-console.log('Type of userRoute:', typeof userRoute); 
-console.log('Type of productRoute:', typeof productRoute); 
-console.log('Type of orderRoute:', typeof orderRoute); 
+console.log('Type of databaseSeeder:', typeof databaseSeeder); // Should be 'function' or 'object'
+console.log('Type of userRoute:', typeof userRoute); // Should be 'function' or 'object'
+console.log('Type of productRoute:', typeof productRoute); // Should be 'function' or 'object'
+console.log('Type of orderRoute:', typeof orderRoute); // Should be 'function' or 'object'
 
+// Ensure PORT is a number
 const PORT = parseInt(process.env.PORT, 10) || 3000;
-
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// Connect to the database
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("Database connected successfully"))
-//   .catch(err => {
-//     console.error("Database connection error:", err);
-//     process.exit(1); // Exit the process with an error code
-//   });
-  const client = new MongoClient(process.env.MONGO_URL, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
+// Connect to the database with SSL options
+mongoose.connect(process.env.MONGO_URL, {
+  
+ 
+  // sslCA: fs.readFileSync('path/to/ca.pem'), // Uncomment and set the path if using a CA certificate
+}).then(() => console.log("Database connected successfully"))
+  .catch(err => {
+    console.error("Database connection error:", err);
+    process.exit(1); // Exit the process with an error code
   });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
 
 // Use routes
 app.use("/api/seed", databaseSeeder);
